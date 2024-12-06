0d6e42fc5c9ecd590b714aa3b106112ab2bca1f2
Matthew Zhang <115035711+mattzhang80@users.noreply.github.com>
2024-12-06 01:09:05 -0500

    Update top.py

--- top.py
+++ top.py
@@ -1,11 +1,4 @@
 #!/usr/bin/env python
-""" top.py Author: Bob Dondero"""
-
-# -----------------------------------------------------------------------
-# top.py
-# Author: Bob Dondero
-# -----------------------------------------------------------------------
-
 import flask
 
 app = flask.Flask("app", template_folder="templates")
